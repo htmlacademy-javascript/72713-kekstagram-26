abe06b805a2aac119fb1bfb43d1397e102dfd039
Евгения <jenny91@bk.ru>
2022-07-06 01:56:33 +0300

    Задание 2, добавлены 2 ф-ии

---
+++ js/main.js
@@ -0,0 +1,16 @@
+function getRandom (min, max) {
+  if (min < 0 || max < 0 || min >= max) {
+    return alert ('Диапазон может быть только положительный. Значение От не может превышать значение До');
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+getRandom (4, 15);
+
+const photoComment = 'папа пошёл в магазин';
+const maxCommentLength = 140;
+function checkCommentLength (comment, maxLength) {
+  if (comment.length > maxLength) {
+    return alert ('Превышена максимальная длина сообщения');
+  }
+}
+checkCommentLength (photoComment, maxCommentLength);
