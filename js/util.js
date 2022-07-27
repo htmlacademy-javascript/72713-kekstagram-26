3e8d7fc068bdf10449dd27709982271ccb94dbf8
Евгения <jenny91@bk.ru>
2022-07-27 21:53:26 +0300

    module11-task1

--- js/util.js
+++ js/util.js
@@ -16,26 +16,25 @@ const isEscapeKey = (evt) => evt.key === 'Escape';
 
 const ALERT_SHOW_TIME = 5000;
 const showAlert = (message) => {
-    const alert=document.createElement('div');
-    alert.style.position='absolute';
-    alert.style.zIndex='100';
-    alert.style.left='0';
-    alert.style.top='0';
-    alert.style.right='0';
-    alert.style.padding='10px 3px';
-    alert.style.fontSize='30px';
-    alert.style.textAlign='center';
-    alert.style.backgroundColor='red';
+  const alert=document.createElement('div');
+  alert.style.position='absolute';
+  alert.style.zIndex='100';
+  alert.style.left='0';
+  alert.style.top='0';
+  alert.style.right='0';
+  alert.style.padding='10px 3px';
+  alert.style.fontSize='30px';
+  alert.style.textAlign='center';
+  alert.style.backgroundColor='red';
 
-    alert.textContent=message;
+  alert.textContent=message;
 
-    document.body.append(alert);
+  document.body.append(alert);
 
-    setTimeout(()=>{
-      alert.remove();
-    }, ALERT_SHOW_TIME);
-  };
+  setTimeout(()=>{
+    alert.remove();
+  }, ALERT_SHOW_TIME);
+};
 
-}
 
 export {getRandom, getRandomArrayElement,checkCommentLength, isEscapeKey, showAlert};
